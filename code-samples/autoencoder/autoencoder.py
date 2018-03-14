a7b0091ddd88fca9a18e9530e2ec788786209580
Peter Bloem <git@peterbloem.nl>
2018-03-14 14:20:55 +0100

    Simplify and add comments

--- code-samples/autoencoder/autoencoder.py
+++ code-samples/autoencoder/autoencoder.py
@@ -1,13 +1,9 @@
 import keras
 
-from keras.models import Sequential, Model
-from keras.layers import Dense, Activation, Conv2D, MaxPool2D, Dropout, Flatten, Input, Reshape
 from keras.optimizers import Adam
 from tensorflow.python.client import device_lib
 
 from sklearn import datasets
-
-import math
 import numpy as np
 
 import matplotlib
@@ -17,45 +13,36 @@ import matplotlib.pyplot as plt
 
 from argparse import ArgumentParser
 
-from keras.layers import Input, Conv2D, Conv2DTranspose, Dense, Reshape, MaxPooling2D, UpSampling2D, Flatten, Cropping2D
-from keras.models import Model, Sequential
+from keras.layers import Dense, Reshape, Flatten
+from keras.models import Sequential
 
+# This function contains all the code
 def go(options):
 
+    # Debugging info to see if we're using the GPU
     print('devices', device_lib.list_local_devices())
 
+    # These are people in the data that smile
     SMILING = [0, 7, 8, 11, 12, 13, 14, 20, 27, 155, 153, 154, 297]
     NONSMILING = [1, 2, 3, 6, 10, 60, 61, 136, 138, 216, 219, 280]
 
-    # faces = datasets.fetch_olivetti_faces()
+    # Dowload the data
     faces = datasets.fetch_lfw_people(data_home='.')
-
-    # smiling/nonsmiling
-    fig = plt.figure(figsize=(5, 3))
-    for i in range(len(SMILING)):
-        ax = fig.add_subplot(3, 5, i + 1, xticks=[], yticks=[])
-        ax.imshow(faces.images[SMILING[i], :, :], cmap='gray')
-
-    plt.savefig('smiling-faces.pdf')
-
-    fig = plt.figure(figsize=(5, 3))
-    for i in range(len(NONSMILING)):
-        ax = fig.add_subplot(3, 5, i + 1, xticks=[], yticks=[])
-        ax.imshow(faces.images[NONSMILING[i], :, :], cmap='gray')
-
-    plt.savefig('nonsmiling-faces.pdf')
+    x = faces.images # x is a 13000 by 67 by 42 array
 
     hidden_size = options.hidden
 
+    # Build the encoder
     encoder = Sequential()
 
-    encoder.add(Flatten(input_shape=(62, 47, 1)))
+    encoder.add(Flatten(input_shape=(62, 47)))
     encoder.add(Dense(1024, activation='relu'))
     encoder.add(Dense(512, activation='relu'))
     encoder.add(Dense(256, activation='relu'))
     encoder.add(Dense(128, activation='relu'))
     encoder.add(Dense(hidden_size))
 
+    # Build the decoder
     decoder = Sequential()
 
     decoder.add(Dense(128, activation='relu', input_dim=hidden_size))
@@ -63,8 +50,9 @@ def go(options):
     decoder.add(Dense(512, activation='relu'))
     decoder.add(Dense(1024, activation='relu'))
     decoder.add(Dense(62*47, activation='relu'))
-    decoder.add(Reshape((62,47,1)))
+    decoder.add(Reshape((62,47)))
 
+    # Stick em together to make the autoencoder
     auto = Sequential()
 
     auto.add(encoder)
@@ -72,39 +60,40 @@ def go(options):
 
     auto.summary()
 
+    # Choose a loss function (MSE) and a search algorithm
+    #         (Adam, a fancy version of gradient descent)
     optimizer = Adam(lr=options.lr)
     auto.compile(optimizer=optimizer, loss='mse')
 
-    x = faces.images[:, :, :, None] / 255.
-
+    # Search for a good model
     auto.fit(x, x,
              epochs=options.epochs,
              batch_size=256, shuffle=True,
-             validation_split=0.1,
-             callbacks=[keras.callbacks.TensorBoard(log_dir='./logs')])
+             validation_split=0.1)
 
-        # out = auto.predict(x[:400, :])
-        #
-        # fig = plt.figure(figsize=(5, 6))
-        #
-        # # plot several images
-        # for i in range(30):
-        #     ax = fig.add_subplot(6, 5, i + 1, xticks=[], yticks=[])
-        #     ax.imshow(out[i, ...].reshape((62, 47)), cmap=plt.cm.gray)
-        #
-        # plt.savefig('faces-reconstructed.{:03d}.pdf'.format(e))
-
+    # Select the smiling and nonsmiling images from the dataset
     smiling = x[SMILING, ...]
     nonsmiling = x[NONSMILING, ...]
 
+    # Pass them through the encoder
     smiling_latent = encoder.predict(smiling)
     nonsmiling_latent = encoder.predict(nonsmiling)
 
+    # Compute the means for both groups
     smiling_mean = smiling_latent.mean(axis=0)
     nonsmiling_mean = nonsmiling_latent.mean(axis=0)
 
+    # Subtract for smiling vector
     smiling_vector = smiling_mean - nonsmiling_mean
 
+
+    # Making somebody smile (person 42):
+    latent = encoder.predict(x[None, 42, ...])
+    l_smile  = latent + 0.3 * smiling_vector
+    smiling = decoder.predict(l_smile)
+
+    # Plot fronwing-to-smiling transition for several people
+    # in a big PDF image
     randos = 6
     k = 9
     fig = plt.figure(figsize=(k, randos))
@@ -130,7 +119,7 @@ if __name__ == "__main__":
     ## Parse the command line options
     parser = ArgumentParser()
 
-    parser.add_argument("-e", "--epochsl",
+    parser.add_argument("-e", "--epochs",
                         dest="epochs",
                         help="Number of epochs.",
                         default=150, type=int)
